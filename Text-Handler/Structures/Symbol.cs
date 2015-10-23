22706e1410ec03ec8a74418a6fe8886151e121b6
Metiska <demenoff.aj@gmail.com>
2015-10-23 23:05:18 +0300

    Structure to save symbols of text.

---
+++ Text-Handler/Structures/Symbol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Text_Handler.Structures
+{
+    public struct Symbol
+    {
+        private string _chars;
+
+        public string Chars
+        {
+            get { return _chars; }
+            set { _chars = value; }
+        }
+
+        public Symbol(string chars)
+        {
+            this._chars = chars;
+        }
+
+        public Symbol(char item)
+        {
+            this._chars = String.Format("{0}",item);
+        }
+
+    }
+}
