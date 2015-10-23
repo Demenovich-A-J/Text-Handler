8f92cbdae6ea51d55b4849b72c7b398ca00dea37
Metiska <demenoff.aj@gmail.com>
2015-10-23 23:57:36 +0300

    new objects of text.

---
+++ Text-Handler/TextObjects/Punctuation.cs
@@ -0,0 +1,23 @@
+﻿using Text_Handler.Interfaces;
+using Text_Handler.Structures;
+
+namespace Text_Handler.TextObjects
+{
+    public class Punctuation : IPunctuation
+    {
+        private Symbol _value;
+        public string Chars
+        {
+            get { return _value.Chars; }
+        }
+        public Symbol Value
+        {
+            get { return this._value; }
+        }
+
+        public Punctuation(string chars)
+        {
+            _value = new Symbol(chars);
+        }
+    }
+}
---
+++ Text-Handler/TextObjects/Sentence.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Text_Handler.Interfaces;
+
+namespace Text_Handler.TextObjects
+{
+    public class Sentence : ISentence
+    {
+        public ICollection<ISenteceItem> Items { get; private set; }
+
+        public Sentence()
+        {
+            Items = new List<ISenteceItem>();
+        }
+
+        public Sentence(IEnumerable<ISenteceItem> items) :this()
+        {
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+}
