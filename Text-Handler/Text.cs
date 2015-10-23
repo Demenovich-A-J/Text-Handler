3359d624c469e556e68798c6536a5dfe83cf4c20
Metiska <demenoff.aj@gmail.com>
2015-10-23 23:58:51 +0300

    Released base functionality.

---
+++ Text-Handler/Text.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Text_Handler.Interfaces;
+
+namespace Text_Handler
+{
+    public class Text
+    {
+        public ICollection<ISentence> Sentences { get; set; }
+
+        public Text()
+        {
+            Sentences = new List<ISentence>();
+        }
+
+        public Text(IEnumerable<ISentence> sentences) : this()
+        {
+            foreach (var sentence in sentences)
+            {
+                Sentences.Add(sentence);
+            }
+        }
+
+    }
+}
