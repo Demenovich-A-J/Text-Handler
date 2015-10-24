2e971c71dc6c54b70add13eec0da4d380b0fe054
Metiska <demenoff.aj@gmail.com>
2015-10-25 01:34:50 +0300

    Separators for help to parse text.

---
+++ Text-Handler/SeparatorContainer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Text_Handler
+{
+    public class SeparatorContainer
+    {
+        private string[] sentenceSeparator = new[] {".", "!", "?"};
+
+        public string[] GetSentenceSeparator()
+        {
+            return sentenceSeparator;
+        } 
+    }
+}
