2a338866087406b1fe1df3e8dc89d79e947d2936
Metiska <demenoff.aj@gmail.com>
2015-10-29 02:40:37 +0300

    added separators. Need it in parser and when return text to the basic understanding.

---
+++ Text-Handler/Separators/PunctuationSeparator.cs
@@ -0,0 +1,23 @@
+﻿namespace Text_Handler.Separators
+{
+    public static class PunctuationSeparator
+    {
+        private static readonly string[] _sentencePunctuationSeparator = {".", "!", "?"};
+        private static readonly string[] _sentenceNoWhiteSpacePunctuation = { "-", "—" };
+        private static readonly string[] _doubleSentencePunctuationSeparator = {"\"", "'" , "(" , "<"};
+        public static string[] SentencePunctuationSeparator
+        {
+            get { return _sentencePunctuationSeparator; }
+        }
+
+        public static string[] DoubleSentencePunctuationSeparator
+        {
+            get { return _doubleSentencePunctuationSeparator; }
+        }
+
+        public static string[] SentenceNoWhiteSpacePunctuation
+        {
+            get { return _sentenceNoWhiteSpacePunctuation; }
+        }
+    }
+}
