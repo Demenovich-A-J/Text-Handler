c2921c14fea92e4b0f5a9d77be635759454317bd
Metiska <demenoff.aj@gmail.com>
2015-10-29 02:39:04 +0300

    move to another folder

---
+++ Text-Handler/Helpers/CollectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Text_Handler.Interfaces;
+
+namespace Text_Handler
+{
+    public static class CollectionHelper
+    {
+        public static void AddRange<T>(this ICollection<T> destination,
+           IEnumerable<T> source)
+        {
+            foreach (T item in source)
+            {
+                destination.Add(item);
+            }
+        }
+    }
+}
