b9326c657c15e83518bca964ec85106e7bba04b4
Metiska <demenoff.aj@gmail.com>
2015-10-29 02:42:36 +0300

    String mass of volwesSeparators to check whether the word begins with a consonant. For Russian language only.

---
+++ Text-Handler/Separators/VolwesSeparator.cs
@@ -0,0 +1,13 @@
+﻿using Text_Handler.Interfaces;
+
+namespace Text_Handler.Separators
+{
+    public static class VolwesSeparator
+    {
+        private static readonly string[] _separator = { "а", "у", "о","ы", "и", "э", "я", "ю", "ё", "е" };
+        public static string[] Separator
+        {
+            get { return _separator; }
+        }
+    }
+}
