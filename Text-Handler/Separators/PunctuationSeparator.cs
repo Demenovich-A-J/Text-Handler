751a054115e23fea5777ccd98337fb676cd77649
Metiska <demenoff.aj@gmail.com>
2015-10-30 00:30:11 +0300

    Added new separators container.

--- Text-Handler/Separators/PunctuationSeparator.cs
+++ Text-Handler/Separators/PunctuationSeparator.cs
@@ -2,9 +2,19 @@
 {
     public static class PunctuationSeparator
     {
+        //TODO rework separators create new groups;
         private static readonly string[] _sentencePunctuationSeparator = {".", "!", "?"};
         private static readonly string[] _sentenceNoWhiteSpacePunctuation = { "—" , ")" };
         private static readonly string[] _doubleSentencePunctuationSeparator = {"\"", "'" , "(" , "<"};
+
+        private static readonly string[] _allSentenceSeparators =
+        {
+            ",", ".", "!", "?", "—", ")", "-", "\"", "'", "(",
+            "<", ">", ";", "[", "]", "{", "}", "‒", "–", "—", 
+            "―", "„", "“", "«", "»", "“", "”", "‘", "’"
+        };
+
+
         public static string[] SentencePunctuationSeparator
         {
             get { return _sentencePunctuationSeparator; }
@@ -18,6 +28,11 @@
         public static string[] SentenceNoWhiteSpacePunctuation
         {
             get { return _sentenceNoWhiteSpacePunctuation; }
+        }
+
+        public static string[] AllSentenceSeparators
+        {
+            get { return _allSentenceSeparators; }
         }
     }
 }
