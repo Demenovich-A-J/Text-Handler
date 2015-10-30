ebd2e0e715970313128dc435f96f07127ff8c5ef
Metiska <demenoff.aj@gmail.com>
2015-10-30 14:06:59 +0300

    Fix bug.

--- Text-Handler/Program.cs
+++ Text-Handler/Program.cs
@@ -9,19 +9,21 @@ using Text_Handler.TextObjects;
 
 namespace Text_Handler
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var a = new TextParser();
 
             var c = a.Parse(new StreamReader("1.txt", Encoding.Default));
 
-            c.ReplaceWordInSentence(0, 6, a.ParseSentence("привет, красавчик").Items.ToList());
+            //c.ReplaceWordInSentence(0, 6, a.ParseSentence("привет, красавчик").Items.ToList());
+
+            //c.ReplaceWordInSentence(0, 6, "привет, красавчик \"Вася\"", a.ParseSentence);
 
             //var d = c.GetSentencesWithoutConsonants(6).ToList().ToList();
 
-            Console.WriteLine(c);
+            Console.WriteLine(c.TextToString());
 
             Console.ReadKey();
         }
