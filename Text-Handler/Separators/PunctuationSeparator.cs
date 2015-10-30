592ada6bdc1048a097e36035fe87bb83d4b1f1a4
Metiska <demenoff.aj@gmail.com>
2015-10-30 13:49:22 +0300

    Reworked all separators

--- Text-Handler/Separators/PunctuationSeparator.cs
+++ Text-Handler/Separators/PunctuationSeparator.cs
@@ -1,38 +1,48 @@
-﻿namespace Text_Handler.Separators
+﻿// ReSharper disable All
+namespace Text_Handler.Separators
 {
     public static class PunctuationSeparator
     {
-        //TODO rework separators create new groups;
-        private static readonly string[] _sentencePunctuationSeparator = {".", "!", "?"};
-        private static readonly string[] _sentenceNoWhiteSpacePunctuation = { "—" , ")" };
-        private static readonly string[] _doubleSentencePunctuationSeparator = {"\"", "'" , "(" , "<"};
-
+        private static readonly string[] _repeatPunctuationSeparator = {"\"", "'"};
+        private static readonly string[] _endPunctuationSeparator = {"!", ".", "?"};
+        private static readonly string[] _innerPunctuationSeparator = { ",", ".", ";", ":"};
+        private static readonly string[] _openPunctuationSeparator = {"<", "(", "[", "{", "„", "«", "‘"};
+        private static readonly string[] _closePunctuationSeparator = {")", ">", "]", "}", "“", "»", "’"};
         private static readonly string[] _allSentenceSeparators =
         {
-            ",", ".", "!", "?", "—", ")", "-", "\"", "'", "(",
-            "<", ">", ";", "[", "]", "{", "}", "‒", "–", "—", 
-            "―", "„", "“", "«", "»", "“", "”", "‘", "’"
+            ",", ".", "!", "?", "—", "-", "\"", "'", "(", ")",
+            "<", ">", ":", ";", "[", "]", "{", "}", "‒", "–", "—", 
+            "―", "„", "“", "«", "»", "‘", "’"
         };
-
-
-        public static string[] SentencePunctuationSeparator
-        {
-            get { return _sentencePunctuationSeparator; }
-        }
-
-        public static string[] DoubleSentencePunctuationSeparator
-        {
-            get { return _doubleSentencePunctuationSeparator; }
-        }
-
-        public static string[] SentenceNoWhiteSpacePunctuation
-        {
-            get { return _sentenceNoWhiteSpacePunctuation; }
-        }
 
         public static string[] AllSentenceSeparators
         {
             get { return _allSentenceSeparators; }
+        }
+
+        public static string[] RepeatPunctuationSeparator
+        {
+            get { return _repeatPunctuationSeparator; }
+        }
+
+        public static string[] EndPunctuationSeparator
+        {
+            get { return _endPunctuationSeparator; }
+        }
+
+        public static string[] InnerPunctuationSeparator
+        {
+            get { return _innerPunctuationSeparator; }
+        }
+
+        public static string[] OpenPunctuationSeparator
+        {
+            get { return _openPunctuationSeparator; }
+        }
+
+        public static string[] ClosePunctuationSeparator
+        {
+            get { return _closePunctuationSeparator; }
         }
     }
 }
