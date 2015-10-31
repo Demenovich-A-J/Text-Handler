c99c8c88b4f38eb1918d152403a88f7a585667db
Metiska <demenoff.aj@gmail.com>
2015-10-31 14:16:36 +0300

    no message

--- Text-Handler/TextObjects/Punctuation.cs
+++ Text-Handler/TextObjects/Punctuation.cs
@@ -1,23 +1,37 @@
-﻿using Text_Handler.Interfaces;
+﻿using System.Linq;
+using System.Text;
+using Text_Handler.Interfaces;
 using Text_Handler.Structures;
 
 namespace Text_Handler.TextObjects
 {
     public class Punctuation : IPunctuation
     {
-        private Symbol _value;
-        public string Chars
+        public Punctuation(string chars)
         {
-            get { return _value.Chars.ToString(); }
-        }
-        public Symbol Value
-        {
-            get { return this._value; }
+            Symbols = chars != null ? chars.Select(x => new Symbol(x)).ToArray() : null;
         }
 
-        public Punctuation(char chars)
+        public string Chars
         {
-            _value = new Symbol(chars);
+            get
+            {
+                var sb = new StringBuilder();
+
+                foreach (var symbol in Symbols)
+                {
+                    sb.Append(symbol.Chars);
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public Symbol[] Symbols { get; private set; }
+
+        public Symbol this[int index]
+        {
+            get { return Symbols[index]; }
         }
     }
 }
