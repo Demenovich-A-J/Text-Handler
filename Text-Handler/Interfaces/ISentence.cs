9129564b79e0ae51fa400d071e21fbe47ba2848c
Metiska <demenoff.aj@gmail.com>
2015-10-31 14:15:08 +0300

    Rework signature of methods add new method.

--- Text-Handler/Interfaces/ISentence.cs
+++ Text-Handler/Interfaces/ISentence.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Text_Handler.Interfaces
 {
     public interface ISentence
     {
         IList<ISentenceItem> Items { get; }
-        bool IsInterrogative();
-        IEnumerable<ISentenceItem> RemoveConsonantsWords(int length);
-        IEnumerable<ISentenceItem> ReplaceWordByElements(int length, IList<ISentenceItem> items);
+        bool IsInterrogative { get; }
+        ISentence RemoveWordsBy(Func<IWord, bool> predicate);
+        IEnumerable<ISentenceItem> ReplaceWordByElements(Func<IWord, bool> predicate, IList<ISentenceItem> items);
+        IEnumerable<IWord> GetWordsWithoutRepetition(int length);
         string SentenceToString();
     }
 }
--- Text-Handler/TextObjects/Sentence.cs
+++ Text-Handler/TextObjects/Sentence.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using Text_Handler.Interfaces;
 using Text_Handler.Separators;
@@ -9,99 +9,100 @@ namespace Text_Handler.TextObjects
 {
     public class Sentence : ISentence
     {
-        public IList<ISentenceItem> Items { get; private set; }
-
-        public bool IsInterrogative()
-        {
-            return Items.Last().Chars == "?";
-        }
-
-        public IEnumerable<ISentenceItem> RemoveConsonantsWords(int length)
-        {
-            for (int i = 0; i < Items.Count; i++)
-            {
-                var word = Items[i] as Word;
-                if (word != null && word.IsСonsonant(VolwesSeparator.Separator) && word.Length == length)
-                {
-                    Items.RemoveAt(i);
-                }
-            }
-
-            return Items;
-        }
-
-        public IEnumerable<ISentenceItem> ReplaceWordByElements(int length, IList<ISentenceItem> items)
-        {
-            for (int i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Chars.Length != length) continue;
-
-                Items.RemoveAt(i);
-
-                foreach (var item in items)
-                {
-                    Items.Insert(i, item);
-                    i++;
-                }
-            }
-
-            return Items;
-        }
-
-
-        private StringBuilder _sb = new StringBuilder();
         private int _i;
-        private void GlueWords(int index)
-        {
-            _i = index;
-            while (true)
-            {
-                _i++;
-                if (_i >= Items.Count) break;
-                _sb.Append(Items[_i].Chars);
-                var nextElement = Items.ElementAtOrDefault(_i + 1);
-                if (nextElement != null)
-                {
-                    if (!PunctuationSeparator.EndPunctuationSeparator.Contains(Items[_i].Chars) &&
-                        !PunctuationSeparator.ClosePunctuationSeparator.Contains(nextElement.Chars) &&
-                        !PunctuationSeparator.InnerPunctuationSeparator.Contains(nextElement.Chars) &&
-                        !PunctuationSeparator.EndPunctuationSeparator.Contains(nextElement.Chars))
-                    {
-                        if (PunctuationSeparator.ClosePunctuationSeparator.Contains(Items[_i].Chars))
-                        {
-                            break;
-                        }
-                        if (PunctuationSeparator.OpenPunctuationSeparator.Contains(Items[_i].Chars) ||
-                            PunctuationSeparator.RepeatPunctuationSeparator.Contains(Items[_i].Chars))
-                        {
-                            GlueWords(_i);
-                        }
-
-                        if (!PunctuationSeparator.ClosePunctuationSeparator.Contains(nextElement.Chars))
-                        {
-                            _sb.Append(" ");
-                        }
-                    }
-                }
-            }
-        }
-
-        public string SentenceToString()
-        {
-            GlueWords(-1);
-            return _sb.ToString();
-        }
 
         public Sentence()
         {
             Items = new List<ISentenceItem>();
         }
 
-        public Sentence(IEnumerable<ISentenceItem> items) :this()
+        public Sentence(IEnumerable<ISentenceItem> items) : this()
         {
             foreach (var item in items)
             {
                 Items.Add(item);
+            }
+        }
+
+        public IList<ISentenceItem> Items { get; private set; }
+
+        public bool IsInterrogative
+        {
+            get { return Items.Last().Chars == "?" || Items.Last().Chars == "?!" || Items.Last().Chars == "!?"; }
+        }
+
+        public ISentence RemoveWordsBy(Func<IWord, bool> predicate)
+        {
+            return new Sentence(Items.Where(x => !(x is IWord && predicate((IWord) x))));
+        }
+
+        public IEnumerable<ISentenceItem> ReplaceWordByElements(Func<IWord, bool> predicate, IList<ISentenceItem> items)
+        {
+            var newSentence = new List<ISentenceItem>();
+
+            foreach (var item in Items)
+            {
+                if (item is IWord && predicate(item as IWord))
+                {
+                    newSentence.AddRange(items);
+                    continue;
+                }
+
+                newSentence.Add(item);
+            }
+
+            return newSentence;
+        }
+
+        public IEnumerable<IWord> GetWordsWithoutRepetition(int length)
+        {
+            return Items.Where(x => x is Word).Cast<Word>().Where(x => x.Length == length);
+        }
+
+        public string SentenceToString()
+        {
+            var sb = new StringBuilder();
+
+            GlueWords(-1, sb);
+            return sb.ToString();
+        }
+
+        private void GlueWords(int index, StringBuilder sb)
+        {
+            _i = index;
+            while (true)
+            {
+                _i++;
+
+                if (_i >= Items.Count) break;
+
+                sb.Append(Items[_i].Chars);
+                var nextElement = Items.ElementAtOrDefault(_i + 1);
+
+                if (nextElement == null) continue;
+
+                if (PunctuationSeparator.EndPunctuationSeparator.Contains(Items[_i].Chars) ||
+                    PunctuationSeparator.OperationPunctuationSeparator.Contains(Items[_i].Chars) ||
+                    PunctuationSeparator.ClosePunctuationSeparator.Contains(nextElement.Chars) ||
+                    PunctuationSeparator.InnerPunctuationSeparator.Contains(nextElement.Chars) ||
+                    PunctuationSeparator.EndPunctuationSeparator.Contains(nextElement.Chars) ||
+                    PunctuationSeparator.OperationPunctuationSeparator.Contains(nextElement.Chars)) continue;
+
+                if (PunctuationSeparator.ClosePunctuationSeparator.Contains(Items[_i].Chars))
+                {
+                    break;
+                }
+
+                if (PunctuationSeparator.OpenPunctuationSeparator.Contains(Items[_i].Chars) ||
+                    PunctuationSeparator.RepeatPunctuationSeparator.Contains(Items[_i].Chars))
+                {
+                    GlueWords(_i, sb);
+                }
+
+                if (!PunctuationSeparator.ClosePunctuationSeparator.Contains(nextElement.Chars))
+                {
+                    sb.Append(" ");
+                }
             }
         }
     }
