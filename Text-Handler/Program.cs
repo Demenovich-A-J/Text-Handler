c9f98a2418994580a86ba2436828ae5566796691
Metiska <demenoff.aj@gmail.com>
2015-10-31 14:18:48 +0300

    Add example how to use objects of my text handler.

--- Text-Handler/Program.cs
+++ Text-Handler/Program.cs
@@ -2,10 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using Text_Handler.Interfaces;
 using Text_Handler.Parser;
-using Text_Handler.TextObjects;
 
 namespace Text_Handler
 {
@@ -13,17 +10,52 @@ namespace Text_Handler
     {
         private static void Main(string[] args)
         {
-            var a = new TextParser();
+            var line = "||============================================================||";
+            var parser = new TextParser();
+            var streamReader = new StreamReader(@"..\..\Files\Text.txt", Encoding.Default);
+            var text = parser.Parse(streamReader);
 
-            var c = a.Parse(new StreamReader("1.txt", Encoding.Default));
+            //1 Вывести все предложения заданного текста в порядке возрастания количества слов в каждом из них.
+            foreach (var sentence in text.GetSentencesInAscendingOrder())
+            {
+                Console.WriteLine(sentence.SentenceToString());
+            }
+            Console.WriteLine(line);
 
-            //c.ReplaceWordInSentence(0, 6, a.ParseSentence("привет, красавчик").Items.ToList());
+            //2 Во всех вопросительных предложениях текста найти и напечатать без повторений слова заданной длины.
+            foreach (var word in text.GetWordsFromInterrogativeSentences(3))
+            {
+                Console.WriteLine(word.Chars);
+            }
+            Console.WriteLine(line);
 
-            //c.ReplaceWordInSentence(0, 6, "привет, красавчик \"Вася\"", a.ParseSentence);
 
-            //var d = c.GetSentencesWithoutConsonants(6).ToList().ToList();
+            //3 Из текста удалить все слова заданной длины, начинающиеся на согласную букву.
+            foreach (var sentence in text.GetSentencesWithoutConsonants(3))
+            {
+                Console.WriteLine(sentence.SentenceToString());
+            }
+            Console.WriteLine(line);
 
-            Console.WriteLine(c.TextToString());
+            //4 В некотором предложении текста слова заданной длины заменить указанной подстрокой, 
+            //длина которой может не совпадать с длиной слова.
+
+            //Есть два варианта
+
+            //1 Передается строка и метод по которому ее нужно распарсить.
+            var variant1 = text.ReplaceWordInSentence(0, 3, "строка, с различными элементами", parser.ParseSentence);
+
+            Console.WriteLine(variant1.SentenceToString());
+
+            Console.WriteLine(line);
+
+            //2 Передается коллекция объектов типа ISentenceItem.
+            var variant2 = text.ReplaceWordInSentence(0, 6,
+                parser.ParseSentence("строка, с различными элементами").Items.ToList());
+
+            Console.WriteLine(variant2.SentenceToString());
+
+            Console.WriteLine(line);
 
             Console.ReadKey();
         }
